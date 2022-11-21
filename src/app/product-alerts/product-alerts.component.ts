9de7f026aa2f812334479c6bf2bcd432c54d41ab
winsonet <winsonet@gmail.com>
2022-11-21 09:31:23 +0800

    2 files modified, 4 added

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css'],
+})
+export class ProductAlertsComponent implements OnInit {
+  constructor() {}
+
+  @Input() product!: Product;
+
+  ngOnInit(): void {}
+}
